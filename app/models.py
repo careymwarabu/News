03c63b4483b4964473754fe38d42ec472d2aae7b
careymwarabu <careyakmwarabu@gmail.com>
2021-06-13 15:57:30 +0300

    add changes on app/models.py

--- app/models.py
+++ app/models.py
@@ -1,31 +1,23 @@
 class Headlines:
-    '''
-    A class that defines the headlines objects
-    '''
-
-    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content):
-
-        self.id = id
-        self.name = name
+    """
+    Class to define headlines objects
+    """
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
         self.author = author
         self.title = title
         self.description = description
         self.url = url
-        self.urlToImage = urlToImage
         self.publishedAt = publishedAt
-        self.content = content
-
+        self.urlToImage = urlToImage
 
 class Sources:
-    '''
-    A class that defines the sources of the news articles
-    '''
+    """
+    Class to define sources objects
+    """
+    def __init__(self, id, name,desc, url, country):
 
-    def __init__(self,id,name,description,url,category,language):
-
-        self.id = "id"
-        self.name =  "name"
-        self.description =  "description"
-        self.url = "https://abcnews.go.com"
-        self.category = "category"
-        self.country = "country"
+        self.id = id
+        self.name = name
+        self.desc = desc
+        self.url = url
+        self.country = country
