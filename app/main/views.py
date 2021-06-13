6490a2c74e9ff4cdeda4b7f243964d9feac32277
careymwarabu <careyakmwarabu@gmail.com>
2021-06-13 15:59:03 +0300

    add app main

--- app/main/views.py
+++ app/main/views.py
@@ -5,23 +5,23 @@ from ..requests import get_articles,get_sources
 # Views
 @main.route('/')
 def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    sources_categories = get_sources('general')
-    title="Welcome to the News Article"
-    return render_template('index.html', title=title,general=sources_categories)
+    title = 'Home - Welcome to News Article'
+    sports = get_sources('sports')
+    business = get_sources('business')
+    entertainment = get_sources('entertainment')
+    general = get_sources('general')
+    health = get_sources('health')
+    science = get_sources('science')
+    technology = get_sources('technology')
+    
+    return render_template('index.html', title=title, sports=sports,business=business,
+                           technology=technology,entertainment=entertainment,
+                           general=general,health=health,science=science,)
 
 
 @main.route('/news/<id>')
 def articles(id):
 
-    '''
-    View news page function that returns the news details page and its data
-    '''
-
-    article_items=get_article(id)
-    title = f'{id}'
-    return render_template('news.html',title = title,articles=article_items)
+    articles_display = get_articles(id)
+    title=f'{id}'
+    return render_template('news.html',articles=articles_display,)
