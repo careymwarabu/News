3288f26d83a241cb8778be8fefa8cce6811d39bc
careymwarabu <careyakmwarabu@gmail.com>
2021-06-13 16:00:05 +0300

    add config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
 
     HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    API_KEY = os.environ.get('API_KEY')
+    API_KEY = os.environ.get('NEWS_API_KEY')
     
 
 class ProdConfig(Config):
